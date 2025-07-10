b42f676f64c85718ac23eb465e344cd83f9f2b55
ruuto102 <ruuto102@gmail.com>
2025-07-10 22:45:52 +0900

    feat: add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar/Navbar.tsx'
 
 function App() {
   return (
-    <div className="text-4xl text-pink-500 font-bold">
-      Hello Tailwind 🎀
+    <div className="min-h-screen">
+      <Navbar />
+      <main className="max-w-screen-md mx-auto p-4">
+        <div className="text-xl text-pink-500 font-bold">
+          Hello Tailwind 🎀
+        </div>
+      </main>
     </div>
   )
 }
