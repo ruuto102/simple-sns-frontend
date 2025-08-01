194549b18b03f4801841414bb08ce81575752bf9
Julian Holfeld <morphclue@gmail.com>
2025-08-01 19:45:04 +0200

    feat: color palette

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <div className="flex items-center gap-3">
         <img src="/logo.png" alt="Logo" className="w-6 h-6 scale-150" />
-      <span className="text-xl font-semibold">Cosbum</span>
+      <span className="text-xl font-bold text-primary">Cosbum</span>
     </div>
   )
 }
